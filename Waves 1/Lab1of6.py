0bfced431a2f0d43b0c11721f3b385d8f21033dd
Ejiro Onose <32671860+Joggyjagz7@users.noreply.github.com>
2020-07-31 12:20:13 +0100

    lab 7 and 8

---
+++ Waves 1/Lab1of6.py
@@ -0,0 +1,9 @@
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message using the variables above.
+print("{}".format(username) + " accessed the site {}".format(url) + " at {} ".format(timestamp))
+
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
