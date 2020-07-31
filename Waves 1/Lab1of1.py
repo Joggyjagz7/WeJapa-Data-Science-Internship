ad9e61c99edfb00eae50d50d4e50edec4474b01d
Ejiro Onose <32671860+Joggyjagz7@users.noreply.github.com>
2020-07-31 12:16:56 +0100

    waves 1

---
+++ Waves 1/Lab1of1.py
@@ -0,0 +1,11 @@
+#Quiz: Average Electricity Bill
+#It's time to try a calculation in Python!
+
+
+
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+
+print()
+avg = (23 + 32 + 64)/3
+print(avg)
---
+++ Waves 1/Lab1of2.py
@@ -0,0 +1,13 @@
+#Quiz: Calculate
+#In this quiz you're going to do some calculations for a tiler. Two parts of a floor need tiling. One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+ floor_part_one = 9 * 7
+ floor_part_two = 5 * 7
+
+ total_titles = floor_part_one + floor_part_two
+ print(total_titles)
+
+ 17_pack = 17 * 6
+ left_over = 17_pack - total_titles
+ print(left_over)
+
+ 
